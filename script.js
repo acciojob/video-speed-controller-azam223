5431ab9adf12d0207161a3983b71f39411e8bb7d
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 18:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const inputs = document.querySelectorAll('.controls input');
-
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
-
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 // Get the video element and controls
 const video = document.querySelector('.flex');
 const progress = document.querySelector('.progress');
